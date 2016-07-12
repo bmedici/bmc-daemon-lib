91c991d52c9f9980893a22997606d7701f616230
Bruno <bmgit@bmconseil.com>
2016-07-12 22:01:17 +0200

    import LoggerPool

--- lib/bmc-daemon-lib.rb
+++ lib/bmc-daemon-lib.rb
@@ -1,4 +1,5 @@
 require_relative "bmc-daemon-lib/conf"
 require_relative "bmc-daemon-lib/logger_formatter"
 require_relative "bmc-daemon-lib/logger_helper"
+require_relative "bmc-daemon-lib/logger_pool"
 # require_relative "bmc-daemon-lib/worker_base"
