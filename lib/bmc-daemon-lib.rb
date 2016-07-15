c1b0dbe9197c44d71959ea5fbbae1ce7497a1a1e
Bruno <bmgit@bmconseil.com>
2016-07-15 17:06:43 +0200

    add requires to load NEwRelic

--- lib/bmc-daemon-lib.rb
+++ lib/bmc-daemon-lib.rb
@@ -1,3 +1,11 @@
+# Global libs
+require "rubygems"
+require "syslog"
+require "thread"
+require "newrelic_rpm"
+
+
+# Project's libs
 require_relative "bmc-daemon-lib/conf"
 require_relative "bmc-daemon-lib/logger_formatter"
 require_relative "bmc-daemon-lib/logger_helper"
